a3039d9983ade6b5fad3899a0f26bbcfc83a9ed8
Cadu Torres <Cadubt@gmail.com>
2021-01-21 22:28:17 -0300

    Creating ScheduleSheet List Endpoint

---
+++ Caritas.ServiceAPI/Repositories/ScheduleSheetRepository.cs
@@ -0,0 +1,24 @@
+﻿using Caritas.ServiceAPI.Context;
+using Caritas.ServiceAPI.Context.Entities;
+using Caritas.ServiceAPI.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Caritas.ServiceAPI.Repositories
+{
+    public class ScheduleSheetRepository : RepositoryBase, IScheduleSheetRepository
+    {
+        public ScheduleSheetRepository(CaritasContext db) : base(db) { }
+
+        public async Task<List<ScheduleSheet>> List()
+        {
+            return await db.ScheduleSheets
+                .AsNoTracking()
+                .Where((s => s.DeletedAt == null))
+                .ToListAsync();
+        }
+    }
+}
--- Caritas.ServiceAPI/Services/ScheduleSheetService.cs
+++ Caritas.ServiceAPI/Services/ScheduleSheetService.cs
@@ -1,4 +1,7 @@
-﻿using Caritas.ServiceAPI.Services.Interfaces;
+﻿using Caritas.ServiceAPI.Context.Entities;
+using Caritas.ServiceAPI.Repositories.Interfaces;
+using Caritas.ServiceAPI.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +11,19 @@ namespace Caritas.ServiceAPI.Services
 {
     public class ScheduleSheetService : IScheduleSheetService
     {
+        private IConfiguration _config { get; set; }
+        private readonly IScheduleSheetRepository _scheduleRepo;
+
+        public ScheduleSheetService(IScheduleSheetRepository scheduleRepo, IConfiguration config)
+        {
+            _scheduleRepo = scheduleRepo;
+            _config = config;
+        }
+        public async Task<List<ScheduleSheet>> List()
+        {
+            List<ScheduleSheet> sheltereds = new List<ScheduleSheet>();
+            sheltereds = await _scheduleRepo.List();
+            return sheltereds;
+        }
     }
 }
